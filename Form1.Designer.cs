8671a8b61809ef6a9a1544f8e87b436ffe04e495
bugaenko-iv <iv.bugaenko@mail.ru>
2024-02-10 19:28:30 +0300

    Добавил кнопки в меню и чуть чуть логики

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,32 +57,56 @@
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
-            // guna2Button1
+            // guna2HtmlLabel1
             // 
-            this.guna2Button1.Animated = true;
-            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.guna2Button1.FillColor = System.Drawing.Color.Transparent;
-            this.guna2Button1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.guna2Button1.ForeColor = System.Drawing.Color.Black;
-            this.guna2Button1.Location = new System.Drawing.Point(362, 216);
-            this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(92, 34);
-            this.guna2Button1.TabIndex = 2;
-            this.guna2Button1.Text = "Играть";
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(372, 247);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(62, 22);
+            this.guna2HtmlLabel1.TabIndex = 3;
+            this.guna2HtmlLabel1.Text = "Играть";
+            this.guna2HtmlLabel1.Click += new System.EventHandler(this.guna2HtmlLabel1_Click);
+            this.guna2HtmlLabel1.MouseEnter += new System.EventHandler(this.guna2HtmlLabel1_MouseEnter);
+            this.guna2HtmlLabel1.MouseLeave += new System.EventHandler(this.guna2HtmlLabel1_MouseLeave);
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(356, 290);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(94, 22);
+            this.guna2HtmlLabel2.TabIndex = 4;
+            this.guna2HtmlLabel2.Text = "Настройки";
+            this.guna2HtmlLabel2.MouseEnter += new System.EventHandler(this.guna2HtmlLabel2_MouseEnter);
+            this.guna2HtmlLabel2.MouseLeave += new System.EventHandler(this.guna2HtmlLabel2_MouseLeave);
+            // 
+            // guna2HtmlLabel3
+            // 
+            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(340, 331);
+            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            this.guna2HtmlLabel3.Size = new System.Drawing.Size(126, 22);
+            this.guna2HtmlLabel3.TabIndex = 5;
+            this.guna2HtmlLabel3.Text = "Выход из игры";
+            this.guna2HtmlLabel3.Click += new System.EventHandler(this.guna2HtmlLabel3_Click);
+            this.guna2HtmlLabel3.MouseEnter += new System.EventHandler(this.guna2HtmlLabel3_MouseEnter);
+            this.guna2HtmlLabel3.MouseLeave += new System.EventHandler(this.guna2HtmlLabel3_MouseLeave);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(816, 482);
-            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.guna2HtmlLabel3);
+            this.Controls.Add(this.guna2HtmlLabel2);
+            this.Controls.Add(this.guna2HtmlLabel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Иг";
+            this.Text = "Главное меню";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -92,7 +118,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Guna.UI2.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,22 +22,66 @@ namespace Гонки
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
         private void label1_Click(object sender, EventArgs e)
-        {
-            
+        {       
         }
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
+        }
 
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void guna2Button1_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void guna2Button1_MouseEnter(object sender, EventArgs e)
+        {
         }
 
         private void guna2HtmlLabel1_Click(object sender, EventArgs e)
         {
-            
+
+        }
+
+        private void guna2HtmlLabel1_MouseEnter(object sender, EventArgs e)
+        {
+            guna2HtmlLabel1.Font = new Font(guna2HtmlLabel1.Font.FontFamily, guna2HtmlLabel1.Font.Size + 2, guna2HtmlLabel1.Font.Style);
+        }
+
+        private void guna2HtmlLabel1_MouseLeave(object sender, EventArgs e)
+        {
+            guna2HtmlLabel1.Font = new Font(guna2HtmlLabel1.Font.FontFamily, guna2HtmlLabel1.Font.Size - 2, guna2HtmlLabel1.Font.Style);
+        }
+
+        private void guna2HtmlLabel2_MouseEnter(object sender, EventArgs e)
+        {
+            guna2HtmlLabel2.Font = new Font(guna2HtmlLabel2.Font.FontFamily, guna2HtmlLabel2.Font.Size + 2, guna2HtmlLabel2.Font.Style);
+        }
+
+        private void guna2HtmlLabel2_MouseLeave(object sender, EventArgs e)
+        {
+            guna2HtmlLabel2.Font = new Font(guna2HtmlLabel2.Font.FontFamily, guna2HtmlLabel2.Font.Size - 2, guna2HtmlLabel2.Font.Style);
+        }
+
+        private void guna2HtmlLabel3_MouseEnter(object sender, EventArgs e)
+        {
+            guna2HtmlLabel3.Font = new Font(guna2HtmlLabel3.Font.FontFamily, guna2HtmlLabel3.Font.Size + 2, guna2HtmlLabel3.Font.Style);
+        }
+
+        private void guna2HtmlLabel3_MouseLeave(object sender, EventArgs e)
+        {
+            guna2HtmlLabel3.Font = new Font(guna2HtmlLabel3.Font.FontFamily, guna2HtmlLabel3.Font.Size - 2, guna2HtmlLabel3.Font.Style);
+        }
+
+        private void guna2HtmlLabel3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
